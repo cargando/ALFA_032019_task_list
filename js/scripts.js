3810d9dad2035c82d121e8cda0eae008a45ea9bb
Sergey Spynu <cargando@mail.ru>
2019-04-28 16:47:38 +0300

    update on 28.04.2019

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,8 @@ var STATE = {
 	}], // - список задач
 	formState: 'add', // [ add, edit, err ] - состояние формы - редактирование или добавление данных
 	editIndex: null, // индекс элемента массива задач для режима редактирования
-
+	calendarVisibility: false, // каледарь отображен/скрыт
+	calendarDate: new Date(), // дата, которую отображает календарь
 	isOpenedPopup: false, // true / false - флаг открыто / закрыто модальное окно
 	formData: { // данные формы
 		taskName: null, // название задачи
@@ -255,4 +256,117 @@ function initPage() {
 	STATE.taskList = tmp || [];
 
 	renderTaskList();
+	renderCalendar(STATE.calendarDate.getFullYear(), STATE.calendarDate.getMonth());
+}
+
+//////////// CALENDAR
+
+function handleShowCalendar(e) {
+	if (STATE.calendarVisibility) {
+		return false;
+	}
+	var calendar = document.getElementById('calendar');
+	var clickedIcon = e.target;
+	var coords = clickedIcon.closest('.input-group-prepend').getBoundingClientRect();
+	// e.target.parentElement.parentElement;
+	calendar.style.top = coords.bottom + "px";
+	calendar.style.left = coords.left + "px";
+	calendar.style.display = 'block';
+	STATE.calendarVisibility = true;
+	echo("handleShowCalendar>> ", coords);
+}
+
+function handleCloseCalendar(e) {
+	var calendar = document.getElementById('calendar');
+	calendar.style.display = 'none';
+	STATE.calendarVisibility = false;
+}
+
+function renderCalendar(yearToOperate, monthToOperate) {
+	var dateToOperate = new Date(yearToOperate, monthToOperate);
+	var year = dateToOperate.getFullYear();
+	var month = dateToOperate.getMonth(); // месяц от 0 до 11, нужно прибавлять 1
+	var dayMonth = new Date().getDate(); // какое число месяца
+	var dayWeek = dateToOperate.getDay(); // от 0 до 6, причем 0 - это воскресение
+	var maximumDaysInPrevMonth = getLastDay(year, month-1);
+	dayWeek = dayWeek === 0 ? 7 : dayWeek;
+	var firstDay = getFirstDayOfMonth(year, month);
+	var j = 1; // это счетчик недель, которые выводятся в календарь
+	var dayCounter = 1;
+	var dayCounterAfter = 1;
+	var str_out_week = '';
+
+	while(j < 7) {
+		var str_out = '';
+		for(var i = 1; i < 8; i++) {
+			var tmpCellObject = {};
+			if ((firstDay.dayWeek > i && j == 1) ) { // если меньше чем 1е число текущего месяца - ячейки для предыдущего месяца
+				var tmpDayMonth = (maximumDaysInPrevMonth + i + 1 - firstDay.dayWeek);
+				tmpCellObject = {
+					className: ' class="not_current"',
+					dataFullDate: (tmpDayMonth + '.' + (month === 0 ? 12 : month) + '.' + (month === 0 ? yearToOperate - 1 : yearToOperate)),
+					dataDaymonth: tmpDayMonth,
+				};
+			} else if (dayCounter > firstDay.maxDays )  { // ячейки для следующего месяца
+				tmpCellObject = {
+					className: ' class="not_current"',
+					dataFullDate: (dayCounterAfter + '.' + (month === 11 ? 1 : month + 2) + '.' + (month == 11 ? yearToOperate + 1 : yearToOperate)),
+					dataDaymonth: dayCounterAfter++,
+				};
+
+			} else { // ЯЧЕЙКИ для ТЕКУЩЕГО МЕСЯЦА
+				var todayClass = '';
+				var currrentDt = new Date();
+
+				if(yearToOperate == currrentDt.getFullYear() && monthToOperate == currrentDt.getMonth()) {
+					todayClass = dayCounter == dayMonth ? ' class="today"' : '';
+				}
+
+				tmpCellObject = {
+					className: todayClass,
+					dataFullDate: (dayCounter + '.' + (month + 1) + '.' + yearToOperate),
+					dataDaymonth: dayCounter++,
+				};
+			}
+			str_out += renderOneCalendarCell(tmpCellObject);
+		}
+		str_out_week += '<tr>' + str_out + '</tr>';
+		j++;
+	}
+
+	renderCalendarMonthHeader(yearToOperate, monthToOperate);
+	document.getElementById('calendar_table').children[1].innerHTML = str_out_week;
+}
+
+function renderCalendarMonthHeader(year, month) {
+	var text = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декаабрь'];
+	document.getElementById("monthHeader").innerHTML = text[month] + ' ' + year;
+
+}
+
+// формирует содержимое одной ячейки календаря и возвращает строку содержащую тег TD и все его содержимое
+function renderOneCalendarCell({ className = null, dataFullDate = null, dataDaymonth = null, cellText = null }) {
+	// if (! (className && dataWeek && dataDaymonth) )
+	if (!className && !dataFullDate && !dataDaymonth ) {
+		return '<td>&nbsp;</td>';
+	}
+	return '<td onclick="handleClickCalendarCell(event)" ' + className + ' data-fulldate="'+ dataFullDate +'" data-daymonth="' + dataDaymonth + '">' +
+		(cellText === null ? dataDaymonth : cellText ) + '</td>';
+}
+
+/* возвращает объект с 2 полями: на какой день недели выпадает первое число месяца и сколько всего в месяце дней*/
+function getFirstDayOfMonth(yy, mm) {
+	var firstDayOfCurrentMonth = new Date(yy, mm, 1); // дата на момент первого числа текущего месяца
+	var month = firstDayOfCurrentMonth.getMonth(); // месяц от 0 до 11, нужно прибавлять 1
+	// var dayMonth = firstDayOfCurrentMonth.getDate();
+	var dayWeek = firstDayOfCurrentMonth.getDay(); // от 0 до 6, причем 0 - это воскресение
+	dayWeek = (dayWeek === 0) ? 7 : dayWeek;
+	return {
+		dayWeek, // номер дня недели первого числа текущего месяца
+		maxDays: getLastDay(yy, mm), // максимальное количество дней  в текуще месяце (который был передан в качестве параметре )
+	}
+}
+
+function getLastDay(yy, mm) {
+	return  new Date(yy, mm +1, 0).getDate();
 }
